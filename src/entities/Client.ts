4a36335aea97edbe43c5a00dbf095378ae5ddfe8
Phuoc Le <lvphuocqt@gmail.com>
2024-07-22 15:24:34 +0700

    test swagger

--- src/entities/Client.ts
+++ src/entities/Client.ts
@@ -11,6 +11,48 @@ import { Banker } from "./Banker";
 import { Transaction } from "./Transaction";
 import { Person } from "./utils/Person";
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CreateClientInput:
+ *      type: object
+ *      required:
+ *        - first_name
+ *        - last_name
+ *        - email
+ *        - card_number
+ *        - balance
+ *      properties:
+ *        email:
+ *          type: string
+ *          default: jane.doe@example.com
+ *        first_name:
+ *          type: string
+ *          default: Jane
+ *        last_name:
+ *          type: string
+ *          default: Doe
+ *        card_number:
+ *          type: string
+ *          default: 1234567890
+ *        balance:
+ *          type: string
+ *          default: 1234567890
+ *    CreateClientResponse:
+ *      type: object
+ *      properties:
+ *        email:
+ *          type: string
+ *        name:
+ *          type: string
+ *        _id:
+ *          type: string
+ *        createdAt:
+ *          type: string
+ *        updatedAt:
+ *          type: string
+ */
 @Entity("client")
 export class Client extends Person {
   @Column({
--- src/routes/create_client.ts
+++ src/routes/create_client.ts
@@ -1,28 +1,47 @@
-import express from 'express';
-import { Client } from '../entities/Client';
+import express from "express";
+import { Client } from "../entities/Client";
 
 const router = express.Router();
 
-router.post('/api/client', async (req, res) => {
-	const {
-		firstName,
-		lastName,
-		email,
-		cardNumber,
-		balance,
-	} = req.body;
+/**
+ * @openapi
+ * '/api/client':
+ *  post:
+ *   tags:
+ *   - Client
+ *   summary: Create client
+ *   requestBody:
+ *     required: true
+ *     contents:
+ *      application/json:
+ *       schema:
+ *            $ref: '#/components/schemas/CreateClientInput'
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CreateClientResponse'
+ *      409:
+ *        description: Conflict
+ *      400:
+ *        description: Bad request
+ */
+router.post("/api/client", async (req, res) => {
+  const { firstName, lastName, email, cardNumber, balance } = req.body;
 
-	const client = Client.create({
-		first_name: firstName,
-		last_name: lastName,
-		email,
-		card_number: cardNumber,
-		balance,
-	});
+  const client = Client.create({
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    card_number: cardNumber,
+    balance,
+  });
 
-	await client.save();
+  await client.save();
 
-	return res.json(client);
+  return res.json(client);
 });
 
 export { router as createClientRouter };
